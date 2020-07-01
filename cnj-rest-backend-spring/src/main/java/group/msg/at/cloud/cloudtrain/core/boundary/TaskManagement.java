09226ffc745965b2aad627d152d245cc870c8945
theism <michael.theis@msg.group>
2020-07-01 12:03:50 +0200

    patched version 4.0.0:
    - spring: integrated downstream service using RestTemplate
    - micro: added missing downstream service values to chart
    - quarkus: added missing downstream service values to chart

--- cnj-rest-backend-spring/src/main/java/group/msg/at/cloud/cloudtrain/core/boundary/TaskManagement.java
+++ cnj-rest-backend-spring/src/main/java/group/msg/at/cloud/cloudtrain/core/boundary/TaskManagement.java
@@ -1,6 +1,7 @@
 package group.msg.at.cloud.cloudtrain.core.boundary;
 
 import group.msg.at.cloud.cloudtrain.core.control.TaskRepository;
+import group.msg.at.cloud.cloudtrain.core.control.UserPermissionVerifier;
 import group.msg.at.cloud.cloudtrain.core.entity.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -22,24 +23,38 @@ public class TaskManagement {
     @Autowired
     private TaskRepository repository;
 
+    @Autowired
+    private UserPermissionVerifier verifier;
+
     @NotNull
     public UUID addTask(@NotNull @Valid Task newTask) {
+        verifier.requirePermission("TASK_CREATE");
         return this.repository.addTask(newTask);
     }
 
     public void modifyTask(@NotNull @Valid Task modifiedTask) {
+        verifier.requirePermission("TASK_UPDATE");
         this.repository.setTask(modifiedTask);
     }
 
     public Optional<Task> getTaskById(@NotNull UUID taskId) {
-        return this.repository.getTaskById(taskId);
+        Optional<Task> result = this.repository.getTaskById(taskId);
+        if (result.isPresent()) {
+            verifier.requirePermission("TASK_READ");
+        }
+        return result;
     }
 
     public void removeTask(@NotNull UUID taskId) {
-        this.repository.removeTaskById(taskId);
+        Optional<Task> found = this.repository.getTaskById(taskId);
+        if (found.isPresent()) {
+            verifier.requirePermission("TASK_DELETE");
+            repository.removeTaskById(found.get().getId());
+        }
     }
 
     public List<Task> getAllTasks() {
+        verifier.requirePermission("TASK_READ");
         return this.repository.getAllTasks();
     }
 }
